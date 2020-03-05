8305b62ffd8110ba23b88e284df3da627ffd4d15
Gui <guilhermehenry60@gmail.com>
2020-03-05 20:29:05 -0300

    feat: add Autorization token

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -22,6 +22,8 @@ export function* signIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -35,7 +37,7 @@ export function* signUp({ payload }) {
   try {
     const { name, email, password } = payload;
 
-    const respose = yield call(api.post, 'users', {
+    yield call(api.post, 'users', {
       name,
       email,
       password,
@@ -49,7 +51,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
