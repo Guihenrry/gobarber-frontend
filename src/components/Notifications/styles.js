6dd3bfe4b1986d43921c8ff0935fd6b84879cc4d
Gui <guilhermehenry60@gmail.com>
2020-03-07 18:08:19 -0300

    feat: add Dashboard

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -43,12 +43,13 @@ export const NotificationList = styled.div`
   width: 250px;
   left: calc(50% - 130px);
   top: calc(100% + 30px);
-  background: rgba(0, 0, 0, 0.6);
+  background: rgba(0, 0, 0, 0.9);
   border-radius: 4px;
   padding: 15px 5px;
   animation-name: ${fadeIn};
   animation-duration: 1s;
   display: ${props => (props.visible ? 'block' : 'none')};
+  z-index: 20;
 
   &::before {
     content: '';
@@ -59,7 +60,7 @@ export const NotificationList = styled.div`
     height: 0;
     border-left: 20px solid transparent;
     border-right: 20px solid transparent;
-    border-bottom: 20px solid rgba(0, 0, 0, 0.6);
+    border-bottom: 20px solid rgba(0, 0, 0, 0.9);
   }
 `;
 
--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,10 +4,8 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      // Key serve para indetificar as aplicações que estaço no storage
       key: 'gobarber',
       storage,
-      // Nome dos reducers que eu quero salvar
       whitelist: ['user', 'auth'],
     },
     reducers
